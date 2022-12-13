f8f11bedf59e8f8d24d5c82145787c04a5eb2aea
Plumbheater <plumbheater@yandex.ru>
2022-12-13 05:24:08 +0300

    initial commit

--- src/test/java/DebitCardOrder.java
+++ src/test/java/DebitCardOrder.java
@@ -1,4 +1,4 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,7 +15,8 @@ public class DebitCardOrder {
 
     @BeforeAll
     static void setUpall() {
-        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+//        WebDriverManager.chromedriver().setup();
     }
 
     @BeforeEach
